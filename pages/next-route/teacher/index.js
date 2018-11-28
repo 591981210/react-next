5d1b5ae39381da3b9a944036a1049aa83d9cf72c
cherish <591981210@qq.com>
2018-11-28 16:09:04 +0800

    路由的查询参数

--- pages/next-route/teacher/index.js
+++ pages/next-route/teacher/index.js
@@ -1,14 +1,33 @@
 import Mynav from '../../../components/Mynav'
 import Mylayout from '../../../layouts/Mylayout'
+import Link from 'next/link'
 
-const teacher = () => (
+const teacherList = [
+    {name: 'jack1', id: 1},
+    {name: 'jack2', id: 2},
+    {name: 'jack3', id: 3}
+]
+const Teacher = () => (
     <div>
-        {/*<Mynav></Mynav>*/}
-        {/*<Mylayout>*/}
-            teacher路由学习
-        {/*</Mylayout>*/}
+        {/* <Mynav></Mynav> */}
+        {/* <Mylayout>
+      <p>teacher页面</p>
+    </Mylayout> */}
+
+        <p>teacher页面</p>
+
+        <ul>
+            {
+                teacherList.map(item => (
+                    <li key={item.id}>
+                        {/* 可以通过Link组件的as属性给路径取别名 */}
+                        <Link  href={`/next-route/teacher/detail?id=${item.id}`}>
+                            <a>{item.name}</a>
+                        </Link>
+                    </li>
+                ))
+            }
+        </ul>
     </div>
-
 )
-
-export default teacher
+export default Teacher
