ee427bd7633cde3a41ef92e431fcd5c2b1c545f7
cherish <591981210@qq.com>
2018-11-28 14:25:55 +0800

    link 组件跳转

--- pages/next-route/index.js
+++ pages/next-route/index.js
@@ -1,5 +1,16 @@
+import Link from 'next/link'
+
 const NextRoute = () =>(
     <div>
+        {/*link 无法样式设置*/}
+        {/*<Link href="/next-route/teacher" style={{color:'red'}}>*/}
+            {/*/!*只允许一个子元素*!/*/}
+            {/*/!*<a>teacher</a>*!/*/}
+            {/*<p style={{color:'red'}}>teacher</p>*/}
+        {/*</Link>*/}
+        <Link href={{pathname:'/next-route/teacher',query:{id:1}}}>
+            <p style={{color:'red'}}>teacher</p>
+        </Link>
         next路由学习
     </div>
 )
